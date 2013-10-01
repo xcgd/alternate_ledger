91b43baf34c8f1503daf7d4e25a0587bb4b7986a
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2013-10-01 12:18:16 +0200

    Remove the type column in ledger types

--- ledger_type.py
+++ ledger_type.py
@@ -1,13 +1,6 @@
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 
-_enum_ledger_type = [
-    ('ledger_a', _('Ledger A')),
-    ('ledger_b', _('Ledger B')),
-    ('ledger_c', _('Ledger C')),
-    ('ledger_d', _('Ledger D')),
-    ('ledger_e', _('Ledger E')),
-]
 
 class ledger_type(osv.Model):
     _name = 'alternate_ledger.ledger_type'
@@ -15,8 +8,6 @@ class ledger_type(osv.Model):
     _columns = {
         'name': fields.char(
             _('Name'), size=256, required=True),
-        'type': fields.selection(
-            _enum_ledger_type, _('Ledger Type'), required=True),
     }
 
     _order = 'name'
