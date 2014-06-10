44f2ea1a47ee8502580853aaf6ca98597d83446a
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-06-10 10:12:56 +0200

    Change version to 1.2.3 (dev)

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Alternate Ledger",
-    "version": "1.2.2",
+    "version": "1.2.3",
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": '''Allow the creation of new accounting ledgers that store
