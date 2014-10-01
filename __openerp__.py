4f3c3833e6927f96c2c6df296ec558d0cffaf17a
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2014-10-01 12:27:10 +0200

    Bumped to 1.5 (stable)(Analytic)

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Alternate Ledger",
-    "version": "1.4",
+    "version": "1.5",
     "author": "XCG Consulting",
     "category": 'Accounting',
     "description": '''Allow the creation of new accounting ledgers that store
